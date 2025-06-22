18cf25b836eed6a35f219d259d0ee965739d8858
vinuth <vinuthsriarampath@outlook.com>
2025-06-22 20:19:26 +0530

    Add message handling in App component using IPC; display received messages and version info

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
-function App() {
-  const notify = () => {
-    // const NOTIFICATION_TITLE = 'Title'
-    // const NOTIFICATION_BODY = 'Notification from the Renderer process. Click to log to console.'
-    // const CLICK_MESSAGE = 'Notification clicked!'
+import { useEffect, useState } from "react"
 
-    window.ipcRenderer.send('notify', 'Title', 'Notification from the Renderer process. Click to log to console.')  
+function App() {
+  const [message, setMessage] = useState('')  
+  useEffect(() => {
+    const handler = (_event: any, msg: string) => {
+      console.log("Received message:", msg);
+      setMessage(msg);
+    }
+  
+    window.ipcRenderer.on('update-message', handler);
+  
+    return () => {
+      window.ipcRenderer.off('update-message', handler);
+    }
+  }, []);
+  
+  const notify = () => {
+    sendNotification('Title', 'Notification from the Renderer process. Click to log to console.');  
+  }
+
+  function sendNotification(title:string, body:string) {
+    window.ipcRenderer.send('notify', title, body)
   }
 
   return (
     <>
-      <div className="flex h-screen w-screen bg-black items-center justify-center">
+      <div className="flex flex-col h-screen w-screen bg-black items-center justify-center">
         <button className="text-white" onClick={notify}>Click To Notify</button>
+        <p className="text-white">{message}</p>
+        <p className="text-white">this is version 0.0.6</p>
       </div>
     </>
   )
