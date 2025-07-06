79448cfdd2aef4350c78166bc0ecbe3b762bb937
vinuth <vinuthsriarampath@outlook.com>
2025-07-06 20:16:18 +0530

    chore : move theme toggle to shared folder under components

--- src/components/shared/mode-toggle.tsx
+++ src/components/shared/mode-toggle.tsx
@@ -2,7 +2,7 @@ import { Moon, Sun } from "lucide-react"
 
 import { Button } from "@/components/ui/button"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger,} from "@/components/ui/dropdown-menu"
-import { useTheme } from "@/components/theme-provider"
+import { useTheme } from "@/components/common/theme-provider"
 
 export function ModeToggle() {
   const { setTheme } = useTheme()
