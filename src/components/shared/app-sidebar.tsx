cca48fdd632dec39db5ff7db1aa27a0c172b0b67
vinuth <vinuthsriarampath@outlook.com>
2025-07-07 01:00:14 +0530

    feature: implement AppSidebar component with user navigation and menu items

---
+++ src/components/shared/app-sidebar.tsx
@@ -0,0 +1,90 @@
+import { Settings, LayoutDashboard, FolderGit2 } from "lucide-react"
+
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarFooter,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    SidebarRail,
+} from "@/components/ui/sidebar"
+import { NavUser } from "../custom/nav-user"
+import { useEffect, useState } from "react"
+import { User } from "@/types/User"
+
+const items = [
+    {
+        title: "Dashboard",
+        url: "/#/app/",
+        icon: LayoutDashboard,
+    },
+    {
+        title: "Projects",
+        url: "/#/app/project",
+        icon: FolderGit2,
+    },
+    {
+        title: "Settings",
+        url: "#",
+        icon: Settings,
+    },
+]
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+    const [user, setUser] = useState<User>({
+        email: '',
+        first_name: '',
+        last_name: '',
+        avatar: ''
+    });
+    useEffect(() => {
+        const userData = localStorage.getItem("user");
+        if (userData) setUser(JSON.parse(userData))
+    }, []);
+
+    return (
+        <Sidebar collapsible="icon" {...props}>
+            <SidebarHeader>
+                <SidebarMenu>
+                    <SidebarMenuItem>
+                        <SidebarMenuButton
+                            asChild
+                            className="data-[slot=sidebar-menu-button]:!p-1.5"
+                        >
+                            <a href="#">
+                                <img src="icon.ico" className="!size-5" />
+                                <span className="text-base font-semibold">Vynex</span>
+                            </a>
+                        </SidebarMenuButton>
+                    </SidebarMenuItem>
+                </SidebarMenu>
+            </SidebarHeader>
+            <SidebarContent>
+                <SidebarGroup>
+                    <SidebarGroupContent>
+                        <SidebarMenu>
+                            {items.map((item) => (
+                                <SidebarMenuItem key={item.title}>
+                                    <SidebarMenuButton asChild>
+                                        <a href={item.url}>
+                                            <item.icon />
+                                            <span>{item.title}</span>
+                                        </a>
+                                    </SidebarMenuButton>
+                                </SidebarMenuItem>
+                            ))}
+                        </SidebarMenu>
+                    </SidebarGroupContent>
+                </SidebarGroup>
+            </SidebarContent>
+            <SidebarFooter>
+                <NavUser user={user} />
+            </SidebarFooter>
+            <SidebarRail />
+        </Sidebar>
+    )
+}
