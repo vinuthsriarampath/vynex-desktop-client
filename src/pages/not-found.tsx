18eebe2ea909f8f467a5d3331b82eead1b7b2202
vinuth <vinuthsriarampath@outlook.com>
2025-07-06 19:54:17 +0530

    feature : add a not found page

---
+++ src/pages/not-found.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button";
+
+function NotFound(){
+    return(
+        <div className="flex h-screen justify-center items-center">
+            <div className="flex flex-col justify-content items-center gap-4 animate-pulse">
+                <img src="logo-v2.png" alt="Login page icon" className="h-24 w-24 animate-bounce" />
+                <p className="text-sm">Page Not Found !!</p>
+                <a href="/"><Button>Go Back</Button></a>
+            </div>
+        </div>
+    )
+}
+
+export default NotFound;
