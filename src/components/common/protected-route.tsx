daf1f4321eadb4e989438ebf2acd7160cad25c49
vinuth <vinuthsriarampath@outlook.com>
2025-07-08 23:11:17 +0530

    feature : authenticate the token using api request

--- src/components/common/protected-route.tsx
+++ src/components/common/protected-route.tsx
@@ -1,11 +1,34 @@
+import { useEffect, useState } from "react";
+import { Navigate, useLocation } from "react-router-dom";
 import { isAuthenticated } from "@/utils/auth";
-import React from "react";
-import { Navigate } from "react-router-dom";
 
-export default function ProtectedRoute({children}:{ children: React.ReactNode}) {
-  if(!isAuthenticated()){
-   return <Navigate to="/" replace />;
-  }  
+export default function ProtectedRoute({ children }: { children: React.ReactNode }) {
+  const [auth, setAuth] = useState<null | boolean>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const location = useLocation(); 
+
+  useEffect(() => {
+    const checkAuthentication = async () => {
+      try {
+        const authenticated = await isAuthenticated();
+        setAuth(authenticated);
+      } catch (error) {
+        setAuth(false);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    checkAuthentication();
+  }, [location.pathname]);
+
+  if (isLoading) {
+    return <div className="flex justify-center items-center">Loading...</div>;
+  }
+
+  if (!auth) {
+    return <Navigate to="/" replace />;
+  }
+
   return <>{children}</>;
-
 }
