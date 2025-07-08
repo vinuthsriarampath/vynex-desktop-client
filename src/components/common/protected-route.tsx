29c25d31d7561d364a589eb990fba87a6731b53d
vinuth <vinuthsriarampath@outlook.com>
2025-07-08 13:46:54 +0530

    fix: correct isAuthenticated function call in ProtectedRoute component

--- src/components/common/protected-route.tsx
+++ src/components/common/protected-route.tsx
@@ -1,10 +1,9 @@
-// components/ProtectedRoute.tsx
 import { isAuthenticated } from "@/utils/auth";
 import React from "react";
 import { Navigate } from "react-router-dom";
 
 export default function ProtectedRoute({children}:{ children: React.ReactNode}) {
-  if(!isAuthenticated){
+  if(!isAuthenticated()){
    return <Navigate to="/" replace />;
   }  
   return <>{children}</>;
