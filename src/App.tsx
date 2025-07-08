2b594f6e4484cb1f795ace6c7bff1d7435690e73
vinuth <vinuthsriarampath@outlook.com>
2025-07-08 23:09:11 +0530

    feature : integrate Toaster component for enhanced notifications in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { HashRouter, } from "react-router-dom";
 import AppRoutes from "./routes";
 import { ThemeProvider } from "./components/common/theme-provider";
+import { Toaster } from "./components/ui/sonner";
 
 function App() {
   return (
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <HashRouter>
+        <Toaster position="top-center" expand={false} richColors />
         <AppRoutes />
       </HashRouter>
     </ThemeProvider>
