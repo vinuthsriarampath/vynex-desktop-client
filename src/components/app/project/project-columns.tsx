dde2c0ee07d1f2cac52efab28c8d5ef8489ffbd8
vinuth <vinuthsriarampath@outlook.com>
2025-07-09 11:38:03 +0530

    feature : add project columns definition for DataTable with sorting and selection capabilities

---
+++ src/components/app/project/project-columns.tsx
@@ -0,0 +1,206 @@
+
+import { ColumnDef } from "@tanstack/react-table"
+
+import { MoreHorizontal } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+import { ArrowUpDown } from "lucide-react"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Checkbox } from "@/components/ui/checkbox"
+import { Project } from "@/types/Project"
+import { toast } from "sonner"
+
+
+export const projectColumns: ColumnDef<Project>[] = [
+    {
+        id: "select",
+        header: ({ table }) => (
+            <Checkbox
+                checked={
+                    table.getIsAllPageRowsSelected() ||
+                    (table.getIsSomePageRowsSelected() && "indeterminate")
+                }
+                onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+                aria-label="Select all"
+            />
+        ),
+        cell: ({ row }) => (
+            <Checkbox
+                checked={row.getIsSelected()}
+                onCheckedChange={(value) => row.toggleSelected(!!value)}
+                aria-label="Select row"
+            />
+        ),
+        enableSorting: false,
+        enableHiding: false,
+    },
+    {
+        accessorKey: "id",
+        header: ({ column }) => {
+            return (
+                <Button
+                    variant="ghost"
+                    onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                >
+                    #
+                    <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+            )
+        },
+    },
+    {
+        accessorKey: "project_name",
+        header: ({ column }) => {
+            return (
+                <Button
+                    variant="ghost"
+                    onClick={() => column.toggleSorting(column.getIsSorted() === "desc")}
+                >
+                    Name
+                    <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+            )
+        },
+    },
+    {
+        accessorKey: "language",
+        header: ({ column }) => {
+            return (
+                <Button
+                    variant="ghost"
+                    onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                >
+                    Language
+                    <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+            )
+        },
+        cell: ({ row }) => row.getValue("language") ? row.getValue("language") : "-",
+    },
+    {
+        accessorKey: "status",
+        header: ({ column }) => {
+            return (
+                <Button
+                    variant="ghost"
+                    onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+                >
+                    Status
+                    <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+            )
+        },
+    },
+    {
+        accessorKey: "show_case",
+        header: ({ column }) => {
+            return (
+                <Button
+                    variant="ghost"
+                    onClick={() => column.toggleSorting(column.getIsSorted() === "desc")}
+                >
+                    Show Case
+                    <ArrowUpDown className="ml-2 h-4 w-4" />
+                </Button>
+            )
+        },
+    },
+    {
+        accessorKey: "createdAt",
+        header: "Created At",
+        cell: ({ row }) => {
+            const value = row.getValue("createdAt");
+            if (!value) return "-";
+            return typeof value === "string" ? value.slice(0, 10) : "-";
+        },
+    },
+    {
+        accessorKey: "updatedAt",
+        header: "Updated At",
+        cell: ({ row }) => {
+            const value = row.getValue("updatedAt");
+            if (!value) return "-";
+            return typeof value === "string" ? value.slice(0, 10) : "-";
+        },
+    },
+
+    // {
+    //     accessorKey: "creadted",
+    //     header: ({ column }) => {
+    //         return (
+    //             <Button
+    //                 variant="ghost"
+    //                 onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+    //             >
+    //                 Email
+    //                 <ArrowUpDown className="ml-2 h-4 w-4" />
+    //             </Button>
+    //         )
+    //     },
+    // },
+    // {
+    //     accessorKey: "amount",
+    //     header: () => <div className="text-right">Amount</div>,
+    //     cell: ({ row }) => {
+    //         const amount = parseFloat(row.getValue("amount"))
+    //         const formatted = new Intl.NumberFormat("en-US", {
+    //             style: "currency",
+    //             currency: "LKR",
+    //         }).format(amount)
+
+    //         return <div className="text-right font-medium">{formatted}</div>
+    //     },
+    // },
+    {
+        id: "actions",
+        cell: ({ row }) => {
+            const project = row.original
+
+            return (
+                <DropdownMenu>
+                    <DropdownMenuTrigger>
+                        <Button className="h-8 w-8 p-0 cursor-pointer">
+                            <span className="sr-only">Open menu</span>
+                            <MoreHorizontal className="h-4 w-4" />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="center">
+                        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                        <DropdownMenuItem
+                            onClick={() => {
+                                navigator.clipboard.writeText(project.html_url);
+                                toast.success("Repository url copied to clipboard!")
+                            }}
+                        >
+                            Github repository url
+                        </DropdownMenuItem>
+                        <DropdownMenuItem
+                            onClick={() => {
+                                window.open(project.html_url, '_blank');
+                            }}
+                        >
+                            View on github
+                        </DropdownMenuItem>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem
+                            onClick={() => {
+                                navigator.clipboard.writeText(project.clone_url);
+                                toast.success("Clone url copied to clipboard!")
+                            }}
+                        >
+                            Github repository url
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            )
+        },
+    },
+
+]
