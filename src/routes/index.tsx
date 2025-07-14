6ce94035c307c1ec5ef8b43c1123ee5f5123cb46
vinuth <vinuthsriarampath@outlook.com>
2025-07-14 12:48:57 +0530

    feature: add account settings route with nested profile and security pages

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,13 @@
 import ProtectedRoute from "@/components/common/protected-route";
+import AccountSettingsPage from "@/features/app/account/account-settings";
 import Dashboard from "@/features/app/dashbord/dashboard";
+import ProfilePage from "@/features/app/account/profile/profile";
 import ProjectPage from "@/features/app/project/project";
 import Landing from "@/features/Landing";
 import MainLayout from "@/layouts/main-layout";
 import NotFound from "@/pages/not-found";
 import { useRoutes } from "react-router-dom";
+import SecurityPage from "@/features/app/account/security/security-page";
 
 export default function AppRoutes() {
   return useRoutes([
@@ -22,6 +25,14 @@ export default function AppRoutes() {
       children: [
         { index: true, element: <Dashboard/> },
         { path: "project", element: <ProjectPage/> },
+        { 
+          path: "account", 
+          element: <AccountSettingsPage/>, 
+          children: [
+            {index:true, element:<ProfilePage/>},
+            {path:"security", element:<SecurityPage/>}
+          ]
+        },
       ],
     },
     {
