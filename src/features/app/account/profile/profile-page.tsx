82c7efbee1ca40fb0d585777e9b9dcde1c4cbc3e
vinuth <vinuthsriarampath@outlook.com>
2025-07-16 07:46:46 +0530

    feature: enhance profile page to utilize user context for dynamic user data display

--- src/features/app/account/profile/profile-page.tsx
+++ src/features/app/account/profile/profile-page.tsx
@@ -5,12 +5,15 @@ import { ProfileInfoItem } from "@/components/shared/profile-info-item";
 import { useState } from "react";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
+import { useUserContext } from "@/contexts/userContext";
 
 
 export default function ProfilePage() {
     const [personalInfoEdit, setPersonalInfoEdit] = useState<boolean>(false);
     const [liveInEdit, setLiveInEdit] = useState<boolean>(false);
     const [bioEdit, setBioEdit] = useState<boolean>(false);
+
+    const { user } = useUserContext();
 
     // Personal Information
     const handlePersonalInfoEdit = () => {
@@ -68,9 +71,9 @@ export default function ProfilePage() {
             <h1 className="font-bold mb-6 text-2xl">My Profile</h1>
             <div className="border border-gray-200/20 rounded-md p-4 mx-1 flex items-center justify-between mb-6">
                 <div className="flex items-center gap-4">
-                    <img src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/38184074.jpg-M4vCjTSSWVw5RwWvvmrxXBcNVU8MBU.jpeg" alt="profile pic" className="rounded-full w-24 h-24 object-cover border border-gray-300" />
+                    <img src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/38184074.jpg-M4vCjTSSWVw5RwWvvmrxXBcNVU8MBU.jpeg" alt="profile pic" className="rounded-full w-24 h-24 object-cover" />
                     <div>
-                        <p className="font-semibold text-lg">Vinuth Sri Arampath</p>
+                        <p className="font-semibold text-lg">{user.first_name } {user.last_name}</p>
                         <p className="text-gray-400">Trainee Software Developer</p>
                         <p className="text-gray-500">@Siygen (Private) Limited</p>
                     </div>
@@ -85,26 +88,24 @@ export default function ProfilePage() {
 
                 {personalInfoEdit ? (
                     <>
-                        <ProfileInfoItem label="First Name" value={<Input form="personal-info-form" className="w-full" type="text" name="first_name" placeholder="First Name" />} />
-                        <ProfileInfoItem label="Last Name" value={<Input form="personal-info-form" className="w-full" type="text" name="last_name" placeholder="Last Name" />} />
-                        <ProfileInfoItem label="Email" value={<Input form="personal-info-form" className="w-full" type="email" name="email" placeholder="Email" />} />
-                        <ProfileInfoItem label="Phone" value={<Input form="personal-info-form" className="w-full" type="tel" name="phone_number" placeholder="Phone Number" />} />
-                        <ProfileInfoItem label="Bio" value={<Textarea form="personal-info-form" className="w-full" name="bio" placeholder="Bio" />} />
+                        <ProfileInfoItem label="First Name" value={<Input form="personal-info-form" value={user.first_name} className="w-full" type="text" name="first_name" placeholder="First Name" />} />
+                        <ProfileInfoItem label="Last Name" value={<Input form="personal-info-form" value={user.last_name} className="w-full" type="text" name="last_name" placeholder="Last Name" />} />
+                        <ProfileInfoItem label="Email" value={<Input form="personal-info-form" value={user.email} className="w-full" type="email" name="email" placeholder="Email" />} />
+                        <ProfileInfoItem label="Phone" value={<Input form="personal-info-form" value={user.contact} className="w-full" type="tel" name="phone_number" placeholder="Phone Number" />} />
                         <div className="col-span-full flex gap-2 mt-2 justify-end">
                             <form id="personal-info-form" className="flex gap-2" onSubmit={handlePersonalInfoSubmit}>
                                 <Button variant="outline" type="button" onClick={handlePersonalInfoEditCancel}>Cancel</Button>
-                                <Button type="reset" variant="secondary" onClick={() => { }}>Clear</Button>
+                                <Button type="reset" variant="secondary">Clear</Button>
                                 <Button type="submit">Update</Button>
                             </form>
                         </div>
                     </>
                 ) : (
                     <>
-                        <ProfileInfoItem label="First Name" value="Vinuth" />
-                        <ProfileInfoItem label="Last Name" value="Sri Arampath" />
-                        <ProfileInfoItem label="Email" value="vinuthsriarampath@outlook.com" />
-                        <ProfileInfoItem label="Phone" value="+94 71 940 1853" />
-                        <ProfileInfoItem label="Bio" value="This is my Bio" />
+                        <ProfileInfoItem label="First Name" value={user.first_name} />
+                        <ProfileInfoItem label="Last Name" value={user.last_name} />
+                        <ProfileInfoItem label="Email" value={user.email} />
+                        <ProfileInfoItem label="Phone" value={user.contact} />
                     </>
                 )}
             </ProfileInfoSection>
@@ -114,26 +115,26 @@ export default function ProfilePage() {
             <section className="border border-gray-200/20 rounded-md p-4 mx-1 mb-4">
                 <div className="flex items-center justify-between mb-4">
                     <h2 className="font-bold text-lg">Bio</h2>
+                    {!bioEdit && 
                     <Button variant="outline" className="rounded-full cursor-pointer" onClick={handleBioEdit}>
                         <PenLine className="mr-1 h-4 w-4" />Edit
                     </Button>
+                    }
                 </div>
                 {bioEdit ? (
                     <>
-                        <Textarea name="bio" placeholder="Bio" />
+                        <Textarea name="bio" placeholder="Bio" value={user.bio} />
                         <div className="col-span-full flex gap-2 mt-2 justify-end">
                             <form id="personal-info-form" className="flex gap-2" onSubmit={handleBioEditSubmit}>
                                 <Button variant="outline" type="button" onClick={handleBioEditCancel}>Cancel</Button>
-                                <Button type="reset" variant="secondary" onClick={() => { }}>Clear</Button>
+                                <Button type="reset" variant="secondary">Clear</Button>
                                 <Button type="submit">Update</Button>
                             </form>
                         </div>
                     </>
                 ) : (
                     <pre className="text-gray-400 whitespace-pre-wrap text-justify">
-                        asodhiu
-                        adssa
-                        asdsad
+                        {user.bio}
                     </pre>
                 )}
             </section>
@@ -143,12 +144,12 @@ export default function ProfilePage() {
             <ProfileInfoSection title="Live in" onEdit={!liveInEdit ? handleLiveInEdit : undefined}>
                 {liveInEdit ? (
                     <>
-                        <ProfileInfoItem label="Address" value={<Textarea name="address" placeholder="Address" />} />
-                        <ProfileInfoItem label="Postal Code" value={<Input name="postal_code" placeholder="Postal Code" />} />
+                        <ProfileInfoItem label="Address" value={<Textarea name="address" placeholder="Address" value={user.address} />} />
+                        <ProfileInfoItem label="Postal Code" value={<Input name="postal_code" placeholder="Postal Code" value={user.postal_code} />} />
                         <div className="col-span-full flex gap-2 mt-2 justify-end">
                             <form id="personal-info-form" className="flex gap-2" onSubmit={handleLiveInEditSubmit}>
                                 <Button variant="outline" type="button" onClick={handleLiveInEditCancel}>Cancel</Button>
-                                <Button type="reset" variant="secondary" onClick={() => { }}>Clear</Button>
+                                <Button type="reset" variant="secondary">Clear</Button>
                                 <Button type="submit">Update</Button>
                             </form>
                         </div>
@@ -156,8 +157,8 @@ export default function ProfilePage() {
 
                 ) : (
                     <>
-                        <ProfileInfoItem label="Address" value="75/6A, kottikawaththa road, gothatuwa new town,2nd lane, Angoda." />
-                        <ProfileInfoItem label="Postal Code" value="10620" />
+                        <ProfileInfoItem label="Address" value={user.address} />
+                        <ProfileInfoItem label="Postal Code" value={user.postal_code} />
                     </>
                 )
                 }
