6d56f0651e503a8784f29de8804f4f0e3eb02062
vinuth <vinuthsriarampath@outlook.com>
2025-07-16 22:07:24 +0530

    chore : add SUPABASE_ANON_KEY and SUPABASE_URL as environment variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,6 +4,8 @@ interface ImportMetaEnv {
     readonly VITE_BASE_URL?: string;
     readonly VITE_GITHUB_PAT?: string;
     readonly VITE_GITHUB_BASE_URL?: string;
+    readonly VITE_SUPABASE_ANON_KEY?: string;
+    readonly VITE_SUPABASE_URL?: string;
   }
   
   interface ImportMeta {
