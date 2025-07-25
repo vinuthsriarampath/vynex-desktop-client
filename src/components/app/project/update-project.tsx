49da499494ad1bf6ffa6eaf7250d1d0a72158c88
vinuth <vinuthsriarampath@outlook.com>
2025-07-25 11:58:52 +0530

    feature: add thumbnail upload functionality to the project update form

--- src/components/app/project/update-project.tsx
+++ src/components/app/project/update-project.tsx
@@ -8,9 +8,11 @@ import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip
 import { Project } from "@/types/Project";
 import axios, { AxiosResponse } from "axios";
 import { CircleQuestionMark, Pencil } from "lucide-react";
-import React, { useState } from "react";
+import React, {useRef, useState} from "react";
 import { toast } from "sonner";
 import {validateProjectForm} from "@/utils/validate-project-form.ts";
+import {CheckBucketExists} from "@/utils/check-bucket.ts";
+import {UploadFile} from "@/utils/upload-file.ts";
 
 interface UpdateProjectInterface {
     setProjects: React.Dispatch<React.SetStateAction<Project[]>>
@@ -19,6 +21,8 @@ interface UpdateProjectInterface {
 export default function UpdateProject({ setProjects, project }: Readonly<UpdateProjectInterface>) {
     const [errors, setErrors] = useState<{ [key: string]: string }>({});
     const [open, setOpen] = useState<boolean>();
+    const [imageUrl, setImageUrl] = useState<string | null>(null);
+    const [selectedFile, setSelectedFile] = useState<File | null>(null);
 
     const GITHUB_PAT = import.meta.env.VITE_GITHUB_PAT ?? " ";
     const GITHUB_BASE_URL = import.meta.env.VITE_GITHUB_BASE_URL;
@@ -26,8 +30,16 @@ export default function UpdateProject({ setProjects, project }: Readonly<UpdateP
     const GITHUB_USERNAME = import.meta.env.VITE_GITHUB_USERNAME ?? " ";
     const TOKEN = localStorage.getItem('token') ? localStorage.getItem('token') : toast.error("Token is missing");
 
+    const inputRef = useRef<HTMLInputElement | null>(null);
+    const dropAreaRef = useRef<HTMLLabelElement | null>(null);
+    const imageViewRef = useRef<HTMLDivElement | null>(null);
+
+    const bucket_name = 'project-thumbnails';
+
     const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
+
+        let thumbnail_url:string = '';
 
         const formData = new FormData(e.currentTarget);
         const project_name = formData.get('project_name') as string;
@@ -41,7 +53,6 @@ export default function UpdateProject({ setProjects, project }: Readonly<UpdateP
         setErrors(errors);
         if (!isValid) return;
 
-
         const toastId = toast.loading("Updating the Repository on GitHub");
 
         try {
@@ -53,8 +64,17 @@ export default function UpdateProject({ setProjects, project }: Readonly<UpdateP
                 return;
             }
 
-            const updatedProject = await updateProjectBackend(project.id, project_name, newRepo.data, showCase, completed);
-            console.log(updatedProject);
+            if(selectedFile){
+                const fileName = generateFileName(selectedFile.name);
+                if (await CheckBucketExists(bucket_name)) {
+                    thumbnail_url=await UploadFile(bucket_name, project.repo_id.toString(), fileName, selectedFile);
+                }else {
+                    toast.error('Bucket does not exist!');
+                }
+            }
+
+            const updatedProject = await updateProjectBackend(project.id, project_name, newRepo.data, showCase, completed,thumbnail_url);
+
             setProjects(prev => prev
                 .map(p => p.id === project.id ? updatedProject.data : p)
                 .sort((a, b) => new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime())
@@ -86,7 +106,7 @@ export default function UpdateProject({ setProjects, project }: Readonly<UpdateP
         );
     };
 
-    const updateProjectBackend = ( projectId: number, project_name: string, repo: { id: number; name: string; html_url: string; description: string; language: string; clone_url: string; created_at: string | Date; updated_at: string | Date; }, showCase: boolean, completed: boolean ) => {
+    const updateProjectBackend = ( projectId: number, project_name: string, repo: { id: number; name: string; html_url: string; description: string; language: string; clone_url: string; created_at: string | Date; updated_at: string | Date; }, showCase: boolean, completed: boolean, thumbnail:string ) => {
         return axios.patch(
             `${BASE_URL}/api/project/update?projectId=${projectId}`,
             {
@@ -99,6 +119,7 @@ export default function UpdateProject({ setProjects, project }: Readonly<UpdateP
                 language: repo.language,
                 clone_url: repo.clone_url,
                 status: completed ? 'completed' : 'in-progress',
+                thumbnail: thumbnail || project.thumbnail,
                 createdAt: new Date(repo.created_at),
                 updatedAt: new Date(repo.updated_at),
             },
@@ -110,6 +131,36 @@ export default function UpdateProject({ setProjects, project }: Readonly<UpdateP
             }
         );
     };
+
+    const handleThumbnailInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files?.[0]){
+            selectImage(e.target.files[0]);
+        }
+    }
+
+    const selectImage = (file: File)=> {
+        const imageUrl = URL.createObjectURL(file);
+        setImageUrl(imageUrl);
+        setSelectedFile(file);
+    }
+
+    const handleDrop = (e: React.DragEvent<HTMLLabelElement>) => {
+        e.preventDefault();
+        if(e.dataTransfer.files?.[0]){
+            selectImage(e.dataTransfer.files[0]);
+            if (inputRef.current) {
+                inputRef.current.files = e.dataTransfer.files;
+            }
+        }
+    }
+
+    const handleDragOver = (e : React.DragEvent<HTMLLabelElement>) => {
+        e.preventDefault();
+    }
+
+    const generateFileName = (name: string) => {
+        return `${project.repo_id}_${Date.now()}.${name.split('.').pop()}`;
+    }
 
     const handleError = (error: unknown, toastId: string | number) => {
         if (axios.isAxiosError(error)) {
@@ -135,6 +186,31 @@ export default function UpdateProject({ setProjects, project }: Readonly<UpdateP
                     </DialogDescription>
                 </DialogHeader>
                 <form onSubmit={handleFormSubmit} className="grid grid-cols items-center gap-4">
+                    <div className="space-y-2 flex justify-center items-center">
+                        <Label id={"image_input"} className="w-85 h-40 cols-span-1 flex items-center border-1 border-gray-600" ref={dropAreaRef} onDrop={handleDrop} onDragOver={handleDragOver}>
+                            <Input type={"file"}  id="image_input" name="image_input" accept=".png, .jpg, .jpeg" hidden ref={inputRef} onChange={handleThumbnailInput} />
+                            <div id={"image_view"}
+                                 ref={imageViewRef}
+                                 className="flex flex-col items-center justify-center text-center w-full h-full bg-center bg-cover overflow-hidden aspect-square"
+                                 style={imageUrl ? { backgroundImage: `url(${imageUrl})` } : { backgroundImage: `url(${project.thumbnail})` }}
+                            >
+                                {!selectedFile && !project.thumbnail&&(
+                                    <>
+                                        <svg id="upload-icon" xmlns="http://www.w3.org/2000/svg" className="w-11 mb-2 fill-gray-500"
+                                             viewBox="0 0 32 32">
+                                            <path
+                                                d="M23.75 11.044a7.99 7.99 0 0 0-15.5-.009A8 8 0 0 0 9 27h3a1 1 0 0 0 0-2H9a6 6 0 0 1-.035-12 1.038 1.038 0 0 0 1.1-.854 5.991 5.991 0 0 1 11.862 0A1.08 1.08 0 0 0 23 13a6 6 0 0 1 0 12h-3a1 1 0 0 0 0 2h3a8 8 0 0 0 .75-15.956z" />
+                                            <path
+                                                d="M20.293 19.707a1 1 0 0 0 1.414-1.414l-5-5a1 1 0 0 0-1.414 0l-5 5a1 1 0 0 0 1.414 1.414L15 16.414V29a1 1 0 0 0 2 0V16.414z" />
+                                        </svg>
+                                        <p className="text-sm">Drag and Drop or click here to upload an image</p>
+                                    </>
+                                )}
+
+                            </div>
+                        </Label>
+                    </div>
+                    <hr/>
                     <div className="space-y-2">
                         <Label htmlFor="project_name">
                             Project Name (Show Case Name)
